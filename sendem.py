2014f94f132d004fd22a6eaa32a8bf23750ba63a
zhangfeihu1990 <zhangfeihu1990@users.noreply.github.com>
2016-09-30 15:35:41 +0800

    Update sendem.py
    发邮件

--- sendem.py
+++ sendem.py
@@ -4,15 +4,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-# 第三方 SMTP 服务
-mail_host="smtp.XXX.com"  #设置服务器
-mail_user="XXXX"    #用户名
-mail_pass="XXXXXX"   #口令 
-
-
 sender = 'from@runoob.com'
 receivers = ['429240967@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
+# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
 message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
 message['From'] = Header("菜鸟教程", 'utf-8')
 message['To'] =  Header("测试", 'utf-8')
@@ -22,9 +17,7 @@ message['Subject'] = Header(subject, 'utf-8')
 
 
 try:
-    smtpObj = smtplib.SMTP() 
-    smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
-    smtpObj.login(mail_user,mail_pass)
+    smtpObj = smtplib.SMTP('localhost')
     smtpObj.sendmail(sender, receivers, message.as_string())
     print ("邮件发送成功")
 except smtplib.SMTPException:
